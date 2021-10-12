182f85a8a2d81601116a178e28719bf98a5a27ee
Metin ÇETİN <metincetin_1996@gmail.com>
2021-10-12 18:36:36 +0300

    Report Page Added

---
+++ src/views/report/Report.js
@@ -0,0 +1,10 @@
+const Report = () => {
+    return (
+        <div class="content">
+            <div class="d-flex justify-content-center">
+                <h1>Report</h1>
+            </div>
+        </div>
+    )
+}
+export default Report
