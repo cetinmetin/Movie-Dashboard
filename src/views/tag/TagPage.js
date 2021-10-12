c7b43ff0185ae87cf0a259b0851f91895d9c234c
Metin ÇETİN <metincetin_1996@gmail.com>
2021-10-12 18:35:59 +0300

    Tag Page Added

---
+++ src/views/tag/TagPage.js
@@ -0,0 +1,10 @@
+const TagPage = () => {
+    return (
+        <div class="content">
+            <div class="d-flex justify-content-center">
+                <h1>TagPage</h1>
+            </div>
+        </div>
+    )
+}
+export default TagPage
