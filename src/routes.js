3d2de6960a021a9006cfdb8ced8bfbac5c7e746d
Metin ÇETİN <metincetin_1996@gmail.com>
2021-10-12 18:42:55 +0300

    Routes for Other Pages Has Been Added

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
-
+const Social = React.lazy(() => import('./views/social/Social'))
+const Report = React.lazy(() => import('./views/report/Report'))
+const TagPage = React.lazy(() => import('./views/tag/TagPage'))
 const routes = [
     { path: '/', exact: true, name: 'Home' },
-    { path: '/dashboard', name: 'Dashboard', component: Dashboard }
+    { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+    { path: '/social', name: 'Social', component: Social },
+    { path: '/report', name: 'Report', component: Report },
+    { path: '/tagPage', name: 'TagPage', component: TagPage }
 ]
 
 export default routes
