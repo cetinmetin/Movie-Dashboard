54798b6bfaf60611aa0bae0cff5bf90af28eabc1
Metin ÇETİN <metincetin_1996@gmail.com>
2021-10-12 18:36:18 +0300

    Social Page Added

---
+++ src/views/social/Social.js
@@ -0,0 +1,11 @@
+
+const Social = () => {
+    return (
+        <div class="content">
+            <div class="d-flex justify-content-center">
+                <h1>Social</h1>
+            </div>
+        </div>
+    )
+}
+export default Social
